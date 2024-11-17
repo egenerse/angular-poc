336a407e1afbde79cca58b82ce2cc04d4f4aa660
Belemir Kürün <kurunbelemir@gmail.com>
2024-11-17 22:15:21 +0100

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.apollon.angular',
+  appName: 'apollon angular mobile',
+  webDir: 'dist/angular-poc/browser'
+};
+
+export default config;
