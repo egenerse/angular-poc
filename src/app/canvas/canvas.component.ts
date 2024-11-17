1756ed162dc74dc4411e3ba77bdb62b6fd7c4a7d
egenerse <egenerse@gmail.com>
2024-11-18 00:07:10 +0100

    fix: firebase deploy

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -3,7 +3,7 @@ import { BoxDirective } from './directives/box.directive';
 import { TriangleDirective } from './directives/triangle.directive';
 import { CanvasElement } from '../canvas-element/canvas-element.interface';
 import { ElementsStore } from '../store/elements.store';
-import { JsonPipe, NgStyle } from '@angular/common';
+import { JsonPipe } from '@angular/common';
 import { DraggableElementDirective } from './directives/draggable-element.directive';
 import { CanvasPanningDirective } from './directives/canvas-panning.directive';
 
@@ -15,55 +15,52 @@ import { CanvasPanningDirective } from './directives/canvas-panning.directive';
     TriangleDirective,
     DraggableElementDirective,
     CanvasPanningDirective,
-    JsonPipe
+    JsonPipe,
   ],
   template: `
-    <!-- [ngStyle]="{ 'touch-action': activeElement ? 'none' : 'auto' }" -->
-
-    <!-- (pointerup)="onCanvasPointerUp($event)"
-    (pointerdown)="onCanvasPointerDown($event)" -->
     <p>
-      
       {{ activeElement | json }}
     </p>
-    <div appCanvasPanning #canvas >
+    <div appCanvasPanning #canvas>
       <div (pointerup)="onCanvasPointerUp($event)">
-      
-      <svg width="2000" height="2000" >
-        @for (element of this.store.elements(); track element.id) {
-        <!-- (pointerdown)="onPointerDown($event, element)" -->
-        <g [attr.transform]="'translate(' + element.x + ',' + element.y + ')'">
-          @if (element.type === 'box') {
+        <svg width="2000" height="2000">
+          @for (element of this.store.elements(); track element.id) {
           <g
-          class="element"
-            appDraggableElement
-            (setActiveElement)="setActiveElement($event)"
-            appBox
-            [activeElement] = "activeElement"
-            [element]="element"
-            [x]="0"
-            [y]="0"
-            [width]="100"
-            [height]="100"
-            [fill]="'#007acc'"
-            [text]="'Box'"
-            [isEditable]="true"
-          ></g>
-          } @else {
-          <g             appDraggableElement
-
-          (setActiveElement)="setActiveElement($event)"
-
-            [activeElement] = "activeElement"
-            [element]="element"
-
-          appTriangle [points]="'50,0 100,100 0,100'" [fill]="'#ff5722'"></g>
+            [attr.transform]="'translate(' + element.x + ',' + element.y + ')'"
+          >
+            @if (element.type === 'box') {
+            <g
+              class="element"
+              appDraggableElement
+              (setActiveElement)="setActiveElement($event)"
+              appBox
+              [activeElement]="activeElement"
+              [element]="element"
+              [x]="0"
+              [y]="0"
+              [width]="100"
+              [height]="100"
+              [fill]="'#007acc'"
+              [text]="'Box'"
+              [isEditable]="true"
+            ></g>
+            } @else {
+            <g
+              class="element"
+              appDraggableElement
+              (setActiveElement)="setActiveElement($event)"
+              [activeElement]="activeElement"
+              [element]="element"
+              appTriangle
+              [points]="'50,0 100,100 0,100'"
+              [fill]="'#ff5722'"
+            ></g>
+            }
+          </g>
           }
-        </g>
-        }
-      </svg>
-    </div>
+        </svg>
       </div>
+    </div>
   `,
   styles: [
     `
--- src/app/canvas/directives/triangle.directive.ts
+++ src/app/canvas/directives/triangle.directive.ts
@@ -1,20 +1,44 @@
-import { Directive, Input, Renderer2, ElementRef, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
 
-@Directive({
+@Component({
   selector: '[appTriangle]',
   standalone: true,
+  template: `
+    <svg:g>
+      <polygon [attr.points]="points()" [attr.fill]="fill()"></polygon>
+      @if(text()) {
+      <text
+        [attr.x]="textX()"
+        [attr.y]="textY()"
+        fill="#000"
+        style="font-size: 14px"
+      >
+        {{ text() }}
+      </text>
+
+      }@if(isEditable()) {
+      <foreignObject
+        [attr.x]="textX() - 20"
+        [attr.y]="textY() - 15"
+        width="50"
+        height="30"
+      >
+        <input
+          type="text"
+          [value]="text()"
+          style="width: 100%; height: 100%; font-size: 14px; border: none; outline: none; padding: 0; box-sizing: border-box;"
+        />
+      </foreignObject>
+      }
+    </svg:g>
+  `,
+  styles: [],
 })
-export class TriangleDirective implements OnInit {
-  @Input() points = '50,0 100,100 0,100';
-  @Input() fill = '#ff5722';
-
-  constructor(private renderer: Renderer2, private host: ElementRef) {}
-
-  ngOnInit() {
-    const polygon = this.renderer.createElement('polygon', 'svg');
-    this.renderer.setAttribute(polygon, 'points', this.points);
-    this.renderer.setAttribute(polygon, 'fill', this.fill);
-
-    this.renderer.appendChild(this.host.nativeElement, polygon);
-  }
+export class TriangleDirective {
+  points = input.required<string>(); // Points for the triangle
+  fill = input.required<string>(); // Fill color
+  text = input(''); // Optional text
+  isEditable = input(false); // Enable input fields
+  textX = input(0); // X-coordinate for the text
+  textY = input(0); // Y-coordinate for the text
 }
